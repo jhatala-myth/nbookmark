31b820a5e2ea5c95c0957466394356141e451cee
Jurij <yuriy@myth.org.ua>
2022-10-06 23:41:48 +0200

    update code

--- app.py
+++ app.py
@@ -4,36 +4,61 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+def sql_query(cursor, query):
+    try:
+        _result = cursor.execute(query)
+    except sqlite3.Error as db_error:
+        return {"status": False, "data": db_error.args}
+    return {"status": True, "data": _result.fetchall()}
+
+
 @app.route('/')
 def index():
+    site_data = {}
     config_query = "SELECT value FROM bookmark_config WHERE item = 'title';"
-    get_query = "SELECT column_id, bookmark_order, column_title FROM bookmark_view ORDER BY column_id;"
+    get_query = "SELECT bookmark_view.tab_id AS tab_index, tab_title, column_id, column_title, bookmark_order " \
+                "FROM bookmark_view LEFT JOIN bookmark_tab ON (bookmark_tab.tab_id=bookmark_view.tab_id)" \
+                "ORDER BY tab_index, column_id;"
+    col_query = "SELECT bookmark_title, bookmark_url FROM bookmark_list WHERE id IN ({})"
+
     ''' DB Connect '''
     db_conn = sqlite3.connect("db/nbookmark.db")
+    db_conn.row_factory = sqlite3.Row
     db_cursor = db_conn.cursor()
-    try:
-        db_cursor.execute(get_query)
-    except sqlite3.Error as db_error:
-        return render_template('error.html', error_msg=' '.join(db_error.args))
+
+    result = sql_query(db_cursor, config_query)
+    if not result['status']:
+        return render_template('error.html', error_msg=' '.join(result['data']))
+
+    site_data['title'] = result['data'][0]['value']
 
     ''' Fetch links from DB '''
-    columns = []
-    records = db_cursor.fetchall()
-    for row in records:
-        '''
-            structure:
-            columns = [ {"title": "column_title", "links": { "bookmark_title": "bookmark_url", ...  }} ] 
-        '''
-        columns.append({"title": row[2], "links": {}})
-        link_query = 'SELECT bookmark_title, bookmark_url FROM bookmark_list WHERE id IN ({})'.format(','.join(row[1].split(',')))
-        db_cursor.execute(link_query)
-        items = db_cursor.fetchall()
-        [columns[len(columns)-1]["links"].update({_[0]: _[1]}) for _ in items]
+    tabs = []
 
-    db_cursor.execute(config_query)
-    config = db_cursor.fetchall()
+    result = sql_query(db_cursor, get_query)
+    if not result['status']:
+        return render_template('error.html', error_msg=' '.join(result['data']))
 
-    return render_template('index.html', url_list=columns, title=config[0][0])
+    print(len(result['data']))
+    for row in result['data']:
+        tabs.append({'tab_id': row['tab_index'], 'tab_title': row['tab_title'], 'columns': []})
+        tab_index = len(tabs) - 1
+        tabs[tab_index]['columns'].append({"title": row['column_title'], "links": {}})
+
+        columns = sql_query(db_cursor, col_query.format(','.join(row['bookmark_order'].split(','))))
+        if not columns['status']:
+            return render_template('error.html', error_msg=' '.join(columns['data']))
+
+        for col in columns['data']:
+            col_index = len(tabs[tab_index]['columns']) - 1
+            '''
+                structure:
+                columns = [ {"title": "column_title", "links": { "bookmark_title": "bookmark_url", ...  }} ] 
+            '''
+            tabs[tab_index]['columns'][col_index]['links'].update({col['bookmark_title']: col['bookmark_url']})
+
+    site_data['tabs'] = tabs
+    return render_template('index.html', url_list=site_data)
 
 
 if __name__ == '__main__':
