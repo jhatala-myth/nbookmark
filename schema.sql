3ecb2f56d7c2ab2d7b699f2fc23b83a7a470bc27
Jurij <yuriy@myth.org.ua>
2022-10-06 23:41:32 +0200

    update sql code

--- schema.sql
+++ schema.sql
@@ -6,15 +6,16 @@ CREATE TABLE IF NOT EXISTS bookmark_list (
 
 CREATE TABLE IF NOT EXISTS bookmark_tab (
  tab_id INTEGER PRIMARY KEY AUTOINCREMENT,
- tab_title TEXT NOT NULL
+ tab_title TEXT UNIQUE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS bookmark_view (
+ tab_id INTEGER NOT NULL,
  column_id INTEGER CHECK (column_id < 4) NOT NULL,
  column_title TEXT NOT NULL,
- tab_id INTEGER NOT NULL,
  bookmark_order TEXT NOT NULL,
- FOREIGN KEY (tab_id) REFERENCES bookmark_tab(tab_id)
+ FOREIGN KEY (tab_id) REFERENCES bookmark_tab(tab_id),
+ UNIQUE (tab_id, column_id)
 );
 
 CREATE TABLE IF NOT EXISTS bookmark_config (
@@ -22,7 +23,7 @@ CREATE TABLE IF NOT EXISTS bookmark_config (
  value TEXT NOT NULL
 );
 
-INSERT INTO bookmark_tab (tab_title) VALUES ('Tab #1');
-INSERT INTO bookmark_list (bookmark_title, bookmark_url) VALUES ('Bookmark #1', 'https://github.com');
-INSERT INTO bookmark_view VALUES (1, 'Column #1', 1, '1');
-INSERT INTO bookmark_config VALUES ('title', 'nBookmark');
+INSERT OR IGNORE INTO bookmark_tab (tab_title) VALUES ('Tab #1');
+INSERT OR IGNORE INTO bookmark_list (bookmark_title, bookmark_url) VALUES ('Bookmark #1', 'https://github.com');
+INSERT OR IGNORE INTO bookmark_view VALUES (1, 1, 'Column #1', '1');
+INSERT OR IGNORE INTO bookmark_config VALUES ('title', 'nBookmark');
