f5bf1bbf8bd6c8589c1c08f6a2af18015060fe1b
Jurij <yuriy@myth.org.ua>
2022-10-06 11:26:02 +0200

    update sql code

--- schema.sql
+++ schema.sql
@@ -4,13 +4,25 @@ CREATE TABLE IF NOT EXISTS bookmark_list (
  bookmark_url TEXT UNIQUE NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS bookmark_tab (
+ tab_id INTEGER PRIMARY KEY AUTOINCREMENT,
+ tab_title TEXT NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS bookmark_view (
- column_id INTEGER CHECK (column_id < 5) NOT NULL UNIQUE,
- column_title TEXT,
- bookmark_order TEXT NOT NULL
+ column_id INTEGER CHECK (column_id < 4) NOT NULL,
+ column_title TEXT NOT NULL,
+ tab_id INTEGER NOT NULL,
+ bookmark_order TEXT NOT NULL,
+ FOREIGN KEY (tab_id) REFERENCES bookmark_tab(tab_id)
 );
 
 CREATE TABLE IF NOT EXISTS bookmark_config (
  item TEXT NOT NULL UNIQUE,
  value TEXT NOT NULL
 );
+
+INSERT INTO bookmark_tab (tab_title) VALUES ('Tab #1');
+INSERT INTO bookmark_list (bookmark_title, bookmark_url) VALUES ('Bookmark #1', 'https://github.com');
+INSERT INTO bookmark_view VALUES (1, 'Column #1', 1, '1');
+INSERT INTO bookmark_config VALUES ('title', 'nBookmark');
